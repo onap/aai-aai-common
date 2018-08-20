a7a1310dc27f720e6c86e60b65da663998a1fcd7
asgar <sammoham@in.ibm.com>
2018-08-20 15:02:27 +0530

    unit test cases for MapperUtil.java
    Issue-ID: AAI-1502
    Change-Id: Ib50774fa7eef41d83bb6a4433fa7e7bcbf83adce
    Signed-off-by: Mohamed Asgar Samiulla <sammoham@in.ibm.com>

--- aai-core/src/test/java/org/onap/aai/util/MapperUtilTest.java
+++ aai-core/src/test/java/org/onap/aai/util/MapperUtilTest.java
@@ -2,7 +2,9 @@
  * ============LICENSE_START=======================================================
  * org.onap.aai
  * ================================================================================
- * Copyright Â© 2017-2018 AT&T Intellectual Property. All rights reserved.
+ * Copyright © 2017-2018 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Modfications Copyright © 2018 IBM.
  * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,54 +19,63 @@
  * limitations under the License.
  * ============LICENSE_END=========================================================
  */
-package org.onap.aai.util; 
- 
+package org.onap.aai.util;
+
+import static org.junit.Assert.assertEquals;
+
 import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-
 public class MapperUtilTest {
 
-	public class SampleClass { 
-	    private String color; 
-	    private String shape; 
- 
-	    public SampleClass(String c, String s){
-	    	color = c; 
-	    	shape = s; 
-	    }
+    public class SampleClass {
+        private String color;
+        private String shape;
 
-		public String getColor() {
-			return color;
-		}
+        public SampleClass(String c, String s) {
+            color = c;
+            shape = s;
+        }
 
-		public void setColor(String color) {
-			this.color = color;
-		}
+        public String getColor() {
+            return color;
+        }
 
-		public String getShape() {
-			return shape;
-		}
+        public void setColor(String color) {
+            this.color = color;
+        }
 
-		public void setShape(String shape) {
-			this.shape = shape;
-		}
-	}
+        public String getShape() {
+            return shape;
+        }
 
-	private JSONObject expectedJson;
+        public void setShape(String shape) {
+            this.shape = shape;
+        }
+    }
 
-	@Before
-	public void setup(){
-       expectedJson = new JSONObject();
-	}
+    private JSONObject expectedJson;
 
-	@Test
-	public void writeAsJSONStringTest() throws Exception {
-	    expectedJson.put("color", "black");
-		expectedJson.put("shape", "box");
-		SampleClass sample = new SampleClass("black", "box");
-		assertEquals(expectedJson.toString(), MapperUtil.writeAsJSONString(sample));
-	} 
+    @Before
+    public void setup() {
+        expectedJson = new JSONObject();
+    }
+
+    @Test
+    public void writeAsJSONStringTest() throws Exception {
+        expectedJson.put("color", "black");
+        expectedJson.put("shape", "box");
+        SampleClass sample = new SampleClass("black", "box");
+        assertEquals(expectedJson.toString(), MapperUtil.writeAsJSONString(sample));
+    }
+
+    @Test
+    public void writeAsJSONStringWithDashesTest() throws Exception {
+        expectedJson.put("color", "black");
+        expectedJson.put("shape", "box");
+        SampleClass sample = new SampleClass("black", "box");
+        assertEquals(expectedJson.toString(), MapperUtil.writeAsJSONStringWithDashes(sample));
+    }
+
 }
