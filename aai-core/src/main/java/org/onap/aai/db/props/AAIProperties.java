8bc89ec6849c3e4956e8c1a55bd4b2762a3aa434
asgar <sammoham@in.ibm.com>
2018-09-05 13:26:39 +0530

    Sonar fixes for AAIProperties.java
    Change-Id: Ie265b619a68db4e116adb6b6cc0510c96c50bd8c
    Issue-ID: AAI-1565
    Signed-off-by: Mohamed Asgar Samiulla <sammoham@in.ibm.com>

--- aai-core/src/main/java/org/onap/aai/db/props/AAIProperties.java
+++ aai-core/src/main/java/org/onap/aai/db/props/AAIProperties.java
@@ -4,6 +4,8 @@
  * ================================================================================
  * Copyright © 2017-2018 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
+ *  Modifications Copyright © 2018 IBM.
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -32,5 +34,8 @@ public class AAIProperties {
 	public static final String LINKED = "linked";
 	public static final String DB_ALIAS_SUFFIX = "-local";
 	public static final String AAI_UUID = "aai-uuid";
-
+	
+    private AAIProperties() {
+    	
+    }
 }
