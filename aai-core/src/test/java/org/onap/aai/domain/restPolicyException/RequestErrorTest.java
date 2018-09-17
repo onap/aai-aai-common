a7494b5e301fe9b81a8a2c6fee28094769dc046f
asgar <sammoham@in.ibm.com>
2018-09-17 21:25:37 +0530

    Test cases added for RequestError.java
    Change-Id: I16d1fbe7ca03ecd6659161ae9bfbf3d4bd5c59be
    Issue-ID: AAI-1610
    Signed-off-by: Mohamed Asgar Samiulla <sammoham@in.ibm.com>

---
+++ aai-core/src/test/java/org/onap/aai/domain/restPolicyException/RequestErrorTest.java
@@ -0,0 +1,53 @@
+/**
+ * ============LICENSE_START=======================================================
+ * org.onap.aai
+ * ================================================================================
+ * Copyright © 2017-2018 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Modifications Copyright © 2018 IBM.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.aai.domain.restPolicyException;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.onap.aai.AAISetup;
+
+public class RequestErrorTest extends AAISetup{
+    private RequestError reqError;
+    
+    @Before
+    public void setup() {
+        reqError = new RequestError();
+    }
+
+    @Test
+    public void testGetAdditionalProperty() throws Exception {
+        reqError.setAdditionalProperty("property1", "value1");
+        assertEquals(reqError.getAdditionalProperties().get("property1"), "value1");
+    }
+
+    @Test
+    public void testGetPolicyException() throws Exception {
+        PolicyException exception = new PolicyException();
+        exception.setMessageId("123");
+        exception.setText("sampletext");
+        reqError.setPolicyException(exception);
+        assertEquals(reqError.getPolicyException().getMessageId(), "123");
+        assertEquals(reqError.getPolicyException().getText(), "sampletext");
+    }
+}
