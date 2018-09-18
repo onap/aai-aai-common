8783c211091674080db5f6af923732138c6f22c5
asgar <sammoham@in.ibm.com>
2018-09-18 20:10:57 +0530

    Added test case for AAIResponseMessageDatum.java
    Change-Id: Ifcc512d8549458fd8c48eb31e2ea128f9f05481d
    Issue-ID: AAI-1623
    Signed-off-by: Mohamed Asgar Samiulla <sammoham@in.ibm.com>

---
+++ aai-core/src/test/java/org/onap/aai/domain/responseMessage/AAIResponseMessageDatumTest.java
@@ -0,0 +1,50 @@
+/**
+ * ============LICENSE_START=======================================================
+ * org.onap.aai
+ * ================================================================================
+ * Copyright © 2017-2018 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Modifications Copyright © 2018 IBM.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.aai.domain.responseMessage;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.onap.aai.AAISetup;
+
+public class AAIResponseMessageDatumTest extends AAISetup{
+    private AAIResponseMessageDatum respMsgDatum;
+    
+    @Before
+    public void setup() {
+        respMsgDatum = new AAIResponseMessageDatum();
+    }
+
+    @Test
+    public void testGetAaiResponseMessageDatumKey() throws Exception {
+        respMsgDatum.setAaiResponseMessageDatumKey("aaiResponseMessageDatumKey");
+        assertEquals(respMsgDatum.getAaiResponseMessageDatumKey(), "aaiResponseMessageDatumKey");
+    }
+    
+    @Test
+    public void testGetAaiResponseMessageDatumValue() throws Exception {
+        respMsgDatum.setAaiResponseMessageDatumValue("aaiResponseMessageDatumValue");
+        assertEquals(respMsgDatum.getAaiResponseMessageDatumValue(), "aaiResponseMessageDatumValue");
+    }
+
+}
