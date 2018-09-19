14b29d9fff4a7111ba6db57a983da55b2cd312fc
asgar <sammoham@in.ibm.com>
2018-09-19 19:49:24 +0530

    test coverage for TransactionLogEntries.java
    Change-Id: Ic405d5000b8334e789cd03138ad70721e3644fbf
    Issue-ID: AAI-1644
    Signed-off-by: Mohamed Asgar Samiulla <sammoham@in.ibm.com>

---
+++ aai-core/src/test/java/org/onap/aai/domain/translog/TransactionLogEntriesTest.java
@@ -0,0 +1,44 @@
+/**
+ * ============LICENSE_START=======================================================
+ * org.onap.aai
+ * ================================================================================
+ * Copyright © 2017-2018 AT&T Intellectual Property. All rights reserved.
+ * ================================================================================
+ * Modifications Copyright © 2018 IBM.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+package org.onap.aai.domain.translog;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.onap.aai.AAISetup;
+
+public class TransactionLogEntriesTest {
+    private TransactionLogEntries logEntries;
+    
+    @Before
+    public void setup() {
+        logEntries = new TransactionLogEntries();
+    }
+
+    @Test
+    public void testGetTransactionLogEntries() throws Exception {
+        assertEquals(logEntries.getTransactionLogEntries(), new ArrayList<TransactionLogEntry>());
+    }
+}
