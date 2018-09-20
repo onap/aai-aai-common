b9c335bab6814a48499d8c6c043a66bd99a01e71
asgar <sammoham@in.ibm.com>
2018-09-20 18:20:26 +0530

    some test cases added
    Change-Id: Iaf98e45665979d156838d1c7596845108b015cc7
    Issue-ID: AAI-1661
    Signed-off-by: Mohamed Asgar Samiulla <sammoham@in.ibm.com>

--- aai-core/src/test/java/org/onap/aai/util/JettyObfuscationConversionCommandLineUtilTest.java
+++ aai-core/src/test/java/org/onap/aai/util/JettyObfuscationConversionCommandLineUtilTest.java
@@ -4,6 +4,8 @@
  * ================================================================================
  * Copyright © 2017-2018 AT&T Intellectual Property. All rights reserved.
  * ================================================================================
+ *  Modifications Copyright © 2018 IBM.
+ * ================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -30,42 +32,65 @@ import org.junit.Test;
 
 public class JettyObfuscationConversionCommandLineUtilTest {
 
-	private final ByteArrayOutputStream testOut = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream testOut = new ByteArrayOutputStream();
 
-	/**
-	 * Test.
-	 */
-	@Test
-	public void test() {
-		//setup, this will catch main's print statements for evaluation
-		PrintStream oldOutputStream = System.out;
-		System.setOut(new PrintStream(testOut));
+    /**
+     * Test.
+     */
+    @Test
+    public void test() {
+        //setup, this will catch main's print statements for evaluation
+        PrintStream oldOutputStream = System.out;
+        System.setOut(new PrintStream(testOut));
 
-		/* ------ TEST OBFUSCATION ----*/
-		JettyObfuscationConversionCommandLineUtil.main(new String[]{"-e", "hello world"});
-		/*
-		 * testOut was also catching any logging statements which interfered with result checking.
-		 * This regex business was the workaround - it tries to find the expected value in
-		 * the results and asserts against that.
-		 */
-		String obfResult = testOut.toString();
-		String obfExpected = "OBF:1thf1ugo1x151wfw1ylz11tr1ymf1wg21x1h1uh21th7";
-		Pattern obfExpectPat = Pattern.compile(obfExpected);
-		Matcher obfMatch = obfExpectPat.matcher(obfResult);
-		assertTrue(obfMatch.find());
+        /* ------ TEST OBFUSCATION ----*/
+        JettyObfuscationConversionCommandLineUtil.main(new String[]{"-e", "hello world"});
+        /*
+         * testOut was also catching any logging statements which interfered with result checking.
+         * This regex business was the workaround - it tries to find the expected value in
+         * the results and asserts against that.
+         */
+        String obfResult = testOut.toString();
+        String obfExpected = "OBF:1thf1ugo1x151wfw1ylz11tr1ymf1wg21x1h1uh21th7";
+        Pattern obfExpectPat = Pattern.compile(obfExpected);
+        Matcher obfMatch = obfExpectPat.matcher(obfResult);
+        assertTrue(obfMatch.find());
 
-		testOut.reset(); //clear out previous result
+        testOut.reset(); //clear out previous result
 
-		/* ------ TEST DEOBFUSCATION ----- */
-		JettyObfuscationConversionCommandLineUtil.main(new String[]{"-d", obfExpected});
-		String deobfResult = testOut.toString();
-		String deobfExpected = "hello world";
-		Pattern deobfExpectPat = Pattern.compile(deobfExpected);
-		Matcher deobfMatch = deobfExpectPat.matcher(deobfResult);
-		assertTrue(deobfMatch.find());
+        /* ------ TEST DEOBFUSCATION ----- */
+        JettyObfuscationConversionCommandLineUtil.main(new String[]{"-d", obfExpected});
+        String deobfResult = testOut.toString();
+        String deobfExpected = "hello world";
+        Pattern deobfExpectPat = Pattern.compile(deobfExpected);
+        Matcher deobfMatch = deobfExpectPat.matcher(deobfResult);
+        assertTrue(deobfMatch.find());
 
-		//clean up, resets to stdout
-		System.setOut(oldOutputStream);
-	}
+        //clean up, resets to stdout
+        System.setOut(oldOutputStream);
+    }
+
+    
+    /**
+     * Test.
+     */
+    @Test
+    public void testUsage() {
+        System.setOut(new PrintStream(testOut));
+
+        /* ------ TEST OBFUSCATION ----*/
+        JettyObfuscationConversionCommandLineUtil.main(new String[]{"-f", "hello world"});
+        /*
+         * testOut was also catching any logging statements which interfered with result checking.
+         * This regex business was the workaround - it tries to find the expected value in
+         * the results and asserts against that.
+         */
+        String obfResult = testOut.toString();
+        assertTrue(obfResult.startsWith("failed to parse input"));
+        
+        testOut.reset(); //clear out previous result
+
+
+    }
 
 }
