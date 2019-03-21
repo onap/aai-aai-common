b246959cee2989220eff5d3e92028c685a201b67
Keong Lim <keong.lim@huawei.com>
2019-03-21 15:34:37 +1100

    AAI-1523 Batch reformat aai-annotations
    Use maven plugins from AAI-2198 to do batch reformat of aai-annotations
    to consistent code style.
    
    Change-Id: If1793b45f688d86b47771c154843be6f071f1df7
    Issue-ID: AAI-1523
    Signed-off-by: Keong Lim <keong.lim@huawei.com>

--- aai-annotations/src/main/java/org/onap/aai/annotations/Metadata.java
+++ aai-annotations/src/main/java/org/onap/aai/annotations/Metadata.java
@@ -8,7 +8,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,6 +17,7 @@
  * limitations under the License.
  * ============LICENSE_END=========================================================
  */
+
 package org.onap.aai.annotations;
 
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
@@ -29,28 +30,50 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD, ElementType.TYPE, ElementType.FIELD})
 public @interface Metadata {
 
-	boolean isKey() default false;
-	String description() default "";
-	String nameProps() default "";
-	String indexedProps() default "";
-	String dependentOn() default "";
-	String container() default "";
-	String namespace() default "";
-	String defaultValue() default "";
-	String searchable() default "";
-	String uniqueProps() default "";
-	String requiredProps() default "";
-	String uriTemplate() default "";
-	String extendsFrom() default "";
-	String isAbstract() default "";
-	String alternateKeys1() default "";
-	String maximumDepth() default "";
-	String crossEntityReference() default "";
-	String requires() default "";
-	String dbAlias() default ""; 
-	String dataLocation() default "";
-	String containsSuggestibleProps() default "";
-	String suggestionAliases() default "";
-	String sourceOfTruthType() default "";
+    boolean isKey() default false;
+
+    String description() default "";
+
+    String nameProps() default "";
+
+    String indexedProps() default "";
+
+    String dependentOn() default "";
+
+    String container() default "";
+
+    String namespace() default "";
+
+    String defaultValue() default "";
+
+    String searchable() default "";
+
+    String uniqueProps() default "";
+
+    String requiredProps() default "";
+
+    String uriTemplate() default "";
+
+    String extendsFrom() default "";
+
+    String isAbstract() default "";
+
+    String alternateKeys1() default "";
+
+    String maximumDepth() default "";
+
+    String crossEntityReference() default "";
+
+    String requires() default "";
+
+    String dbAlias() default "";
+
+    String dataLocation() default "";
+
+    String containsSuggestibleProps() default "";
+
+    String suggestionAliases() default "";
+
+    String sourceOfTruthType() default "";
 
 }
