4b76b6de0d39d6c5aca8635c78574c520f1053e9
Keong Lim <keong.lim@huawei.com>
2019-03-21 15:34:15 +1100

    AAI-1523 Batch reformat aai-auth
    Use maven plugins from AAI-2198 to do batch reformat of aai-auth
    to consistent code style.
    
    Change-Id: I7ea7c6e991b81b3d8b167bdd0889aa5674a42001
    Issue-ID: AAI-1523
    Signed-off-by: Keong Lim <keong.lim@huawei.com>

--- aai-auth/src/main/java/org/onap/aaiauth/auth/Auth.java
+++ aai-auth/src/main/java/org/onap/aaiauth/auth/Auth.java
@@ -9,7 +9,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,6 +18,7 @@
  * limitations under the License.
  * ============LICENSE_END=========================================================
  */
+
 package org.onap.aaiauth.auth;
 
 import org.apache.http.cookie.Cookie;
@@ -39,7 +40,8 @@ public class Auth {
     }
 
     /**
-     *  Returns true if the user is allowed to access a function.
+     * Returns true if the user is allowed to access a function.
+     * 
      * @param authUser
      *        - String value of the user.
      * @param authAction
