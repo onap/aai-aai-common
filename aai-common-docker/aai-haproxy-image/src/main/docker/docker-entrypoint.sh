1833cf56285fa9c6d1937a6cc659b1ca2d91ddec
Harish Venkata Kajur <vk250x@att.com>
2020-07-15 13:16:23 -0400

    Update haproxy to run as user haproxy
    Issue-ID: AAI-2822
    Change-Id: I586c5da9c6a8710536b106272447806d3bfed3b4
    Signed-off-by: Harish Venkata Kajur <vk250x@att.com>

--- aai-common-docker/aai-haproxy-image/src/main/docker/docker-entrypoint.sh
+++ aai-common-docker/aai-haproxy-image/src/main/docker/docker-entrypoint.sh
@@ -16,7 +16,7 @@ sed -i 's/${ONAP_NAMESERVER_CLUSTER_IP}/'${NAMESERVER_IP}'/g' /usr/local/etc/hap
 if [ "$1" = 'haproxy' ]; then
     # if the user wants "haproxy", let's use "haproxy-systemd-wrapper" instead so we can have proper reloadability implemented by upstream
     shift # "haproxy"
-    set -- "$(which haproxy-systemd-wrapper)" -p /run/haproxy.pid "$@"
+    set -- "$(which haproxy-systemd-wrapper)" -p /usr/local/etc/haproxy/haproxy.pid "$@"
 fi
 
 exec "$@"
