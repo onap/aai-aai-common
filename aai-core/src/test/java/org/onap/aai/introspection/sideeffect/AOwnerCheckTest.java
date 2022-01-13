58c1f27f20566cf27afac386f8ec9bfb6fc79d27
wr148d <wr148d@att.com>
2022-01-13 09:39:16 -0500

    [AAI] Revert some testing changes to ignore tests and rollback of eclipse perisistence
    Issue-ID: AAI-3428
    Change-Id: Ib1a1235d44c1c0ddc767ad944eccafec7de618d1
    Signed-off-by: wr148d <wr148d@att.com>

--- aai-core/src/test/java/org/onap/aai/introspection/sideeffect/AOwnerCheckTest.java
+++ aai-core/src/test/java/org/onap/aai/introspection/sideeffect/AOwnerCheckTest.java
@@ -52,12 +52,10 @@ import org.onap.aai.serialization.db.DBSerializer;
 import org.onap.aai.serialization.engines.JanusGraphDBEngine;
 import org.onap.aai.serialization.engines.QueryStyle;
 import org.onap.aai.serialization.engines.TransactionalGraphEngine;
-import org.junit.Ignore;
 
 @RunWith(value = Parameterized.class)
 
-@Ignore
-public class OwnerCheckTest extends AAISetup {
+public class AOwnerCheckTest extends AAISetup {
 
     private static JanusGraph graph;
     private final static ModelType introspectorFactoryType = ModelType.MOXY;
