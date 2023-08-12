f218a598104c6d40b47320f6972aeb262979372e
Fiete Ostkamp <Fiete.Ostkamp@telekom.de>
2023-08-12 09:53:30 +0000

    Refactor HttpEntryTest
    - make test cases independently runnable by removing shared state
    - let method naming better reflect what is being tested
    - assign commonly used objects on the class level when they are not modified in the test case
    - parse error response object and assert error messages
    
    Issue-ID: AAI-3658
    Change-Id: I3b9704b8a6b6cfa6ecf4930dc3b1d1a7ce84764a
    Signed-off-by: Fiete Ostkamp <Fiete.Ostkamp@telekom.de>

---
+++ aai-core/src/test/java/org/onap/aai/rest/db/ServiceException.java
@@ -0,0 +1,53 @@
+/**
+ * ============LICENSE_START=======================================================
+ * org.onap.aai
+ * ================================================================================
+ * Copyright © 2023 Deutsche Telekom. All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ */
+
+package org.onap.aai.rest.db;
+
+import java.util.List;
+
+public class ServiceException {
+    private String messageId;
+    private String text;
+    private List<String> variables;
+
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(String messageId) {
+        this.messageId = messageId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public List<String> getVariables() {
+        return variables;
+    }
+
+    public void setVariables(List<String> variables) {
+        this.variables = variables;
+    }
+}
